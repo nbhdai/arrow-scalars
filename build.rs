25809274fb0d5b34b82fc5e5dc79243c88c424aa
Sven Cattell <scattell@gmail.com>
2023-01-10 10:16:36 -0500

    Serde support

--- build.rs
+++ build.rs
@@ -1,6 +1,20 @@
+#[cfg(feature = "serde")]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //std::env::set_var("OUT_DIR", "src");
+    tonic_build::configure()
+        .build_server(false)
+        .out_dir("src/") // you can change the generated code's location
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(
+            &["./arrow_scalars.proto"],
+            &["./"], // specify the root location to search proto dependencies
+        )?;
 
+    Ok(())
+}
+#[cfg(not(feature = "serde"))]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //std::env::set_var("OUT_DIR", "src");
     tonic_build::configure()
         .build_server(false)
         .out_dir("src/") // you can change the generated code's location
